fbf36bb55758b09e8125002cf88ff86b7ff3c260
Sagar Bhusal <127485363+sagar479@users.noreply.github.com>
2024-07-29 00:52:43 +0545

    Update action.ts

--- utils/action.ts
+++ utils/action.ts
@@ -1,46 +1,25 @@
-/*
- * Install the Generative AI SDK
- *
- * $ npm install @google/generative-ai
- *
- * See the getting started guide for more information
- * https://ai.google.dev/gemini-api/docs/get-started/node
- */
+import { GoogleGenerativeAI } from "@google/generative-ai";
 
-import { GoogleGenerativeAI, HarmCategory, HarmBlockThreshold, GenerativeModel } from "@google/generative-ai";
-
-const apiKey: string = "AIzaSyCNxL1UQlsV8ojQNJO4ViU3SyjqrVrr7Yw";
-const genAI = new GoogleGenerativeAI(apiKey);
-
-const model: GenerativeModel = genAI.getGenerativeModel({
-  model: "tunedModels/corex-ai-nmtokm4xescc",
-});
-
-const generationConfig = {
-  temperature: 0.9,
-  topP: 1,
-  maxOutputTokens: 8192,
-  responseMimeType: "text/plain",
-};
-
-interface Part {
-  text: string;
+interface Chat {
+    role: "user" | "model";
+    parts: string;
 }
 
-export async function run(userPrompt: string, history: Part[]): Promise<string> {
-  const parts: Part[] = [
-    ...history,
-    { text: `input: ${userPrompt}` },
-  ];
+const genAI = new GoogleGenerativeAI("AIzaSyCbxLs9SA2E7f9_rDvTbbJUU1_dfbBxRRU");
 
-  try {
-    const result = await model.generateContent({
-      contents: [{ role: "user", parts }],
-      generationConfig,
-    });
-    return result.response.text;
-  } catch (error) {
-    console.error('Error generating content:', error);
-    return "An error occurred while generating the response.";
-  }
+export async function run(prompt: string, history: Chat[]) {
+    const model = genAI.getGenerativeModel({ model: "tunedModels/corex-ai-nmtokm4xescc" })
+
+    const chat = model.startChat({
+        history: history,
+        generationConfig: {
+            maxOutputTokens: 1000,
+        }
+    })
+
+    const result = await chat.sendMessage(prompt);
+    const response = await result.response;
+    const output = response.text()
+
+    return output
 }
