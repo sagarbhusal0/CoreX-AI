46507da68ab49935cc9a337dac0f203ee6e81676
Sagar Bhusal <127485363+sagar479@users.noreply.github.com>
2024-07-29 13:28:36 +0545

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <link rel="icon" href="https://iili.io/dTIogbS.png" />
       <body className={inter.className}>
         <div className="bg-[#212121] w-full">
           {children}
