08c9bad1831be4e7edc1e779ff698ca5b9463d7d
Sagar Bhusal <127485363+sagarbhusal0@users.noreply.github.com>
2024-12-10 22:43:52 +0545

    Update InitialUI.tsx

--- components/InitialUI.tsx
+++ components/InitialUI.tsx
@@ -7,7 +7,7 @@ const InitialUI = () => {
             <Image className='h-auto w-96 object-cover' src="/logo.png" alt='Image' height={400} width={400} />
             <h1 className='text-4xl text-center font-bold gradient-text'>Meet CoreX AI</h1>
             
-            <p className='text-lg text-gray-400 text-center'>Welcome to CoreX AI, your smart chatbot designed to make conversations easy and helpful. Like ChatGPT, CoreX AI can chat with you, answer questions, and provide support. Our goal is to make interacting with technology simple and enjoyable. Whether you need information, help, or just a friendly chat, CoreX AI is here for you. Experience the future of chatting with CoreX AI!.</p>
+            <p className='text-lg text-gray-400 text-center'>Welcome to CoreX AI, your smart chatbot designed to make conversations easy and helpful. Like ChatGPT, CoreX AI can chat with you, answer questions, provide support And Some Image Related work. My goal is to make interacting with technology simple and enjoyable. Whether you need information, help, or just a friendly chat, CoreX AI is here for you. Experience the future of chatting with CoreX AI!.</p>
             <p>© Sagar Bhusal</p>
         </div>
     ) 
